28622cdbbe74ec7ddeda5ccea6a161b599bc0e44
administrator <csokt0@gmail.com>
2017-09-06 10:00:48 +0200

    Initial commit

---
+++ imports/startup/server/logger.js
@@ -0,0 +1,17 @@
+import bunyan from 'bunyan'
+
+//log = bunyan.createLogger({name: 'TIR'})
+
+log = bunyan.createLogger({
+    name: 'LIR',
+//    level: 'info',
+    level: 'debug',
+    streams: [{
+        type: 'rotating-file',
+        path: '/home/administrator/lir.log',
+        period: '1d',
+        count: 3
+    }]
+})
+
+log.info('Program started.')
